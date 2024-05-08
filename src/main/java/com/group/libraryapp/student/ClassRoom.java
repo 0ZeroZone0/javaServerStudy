31d82870a6526d15d987c5e2f2731ad83240c52e
0Zone <00zero0zone00@gmail.com>
2024-05-09 00:22:38 +0900

    문자열 SQL을 직접 사용하는 것이 너무 어렵다

---
+++ src/main/java/com/group/libraryapp/student/ClassRoom.java
@@ -0,0 +1,9 @@
+package com.group.libraryapp.student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassRoom {
+    private String name;
+    private List<Student> student = new ArrayList<>();
+}
---
+++ src/main/java/com/group/libraryapp/student/Student.java
@@ -0,0 +1,7 @@
+package com.group.libraryapp.student;
+
+public class Student {
+
+    private String name;
+    private ClassRoom classRoom;
+}
