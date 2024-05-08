50b88cd8625d7d39417f2bc83467de04f16718e4
0Zone <00zero0zone00@gmail.com>
2024-05-09 00:40:48 +0900

    유저 테이블에 대응되는 Entity Class 만들기

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,13 +1,19 @@
 package com.group.libraryapp.domain.user;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class User {
 
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id = null;
+  @Column(nullable = false, length = 20) //name varchar(20)
   private String name;
   private Integer age;
 
+  protected User(){}
 
   public User(String name, Integer age) {
     if (name == null || name.isBlank()) {
