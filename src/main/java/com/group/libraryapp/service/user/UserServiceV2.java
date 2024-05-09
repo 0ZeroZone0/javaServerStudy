7df4cfdfed836f1f7b6185de462f8586609bed6c
0Zone <00zero0zone00@gmail.com>
2024-05-10 00:36:15 +0900

    트랜잭션 적용과 영속성 컨텍스트

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,10 +20,16 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    // 아래 있는 함수가 시작될때 start transaction; 을 해준다 (트랜잭션 시작!)
+    // 함수가 예외 없이 잘 끝났다면 commit
+    // 혹시라도 문제가 있다면 rollback
+
+    @Transactional
     public void saveUser(UserCreateRequest userCreateRequest){
         userRepository.save(new User(userCreateRequest.getName(), userCreateRequest.getAge()));
     }
 
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers(){
         List<User> users = userRepository.findAll();
         return users.stream()
@@ -30,6 +37,7 @@ public class UserServiceV2 {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest userUpdateRequest){
         // select * from user where id = ?;
         // Optional<User>
@@ -37,10 +45,11 @@ public class UserServiceV2 {
                 .orElseThrow(IllegalAccessError::new);
 
         user.updateName(userUpdateRequest.getName());
-        userRepository.save(user);
+        //userRepository.save(user);
     }
 
 
+    @Transactional
     public void deleteUser(String name){
         // SELECT * FROM user WHERE name = ?
         User user = userRepository.findByName(name).orElseThrow(IllegalArgumentException::new);
